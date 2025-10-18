0a0146cddbb26f81c66caed137620842ef904942
Pulkit Verma <pulkitverma4567@gmail.com>
2025-10-19 01:04:01 +0530

    Login and signUp Data Scheme Created

---
+++ frontend/composeApp/src/commonMain/kotlin/org/rajat/quickpick/domain/modal/auth/RegisterUserRequest.kt
@@ -0,0 +1,12 @@
+package org.rajat.quickpick.domain.modal.auth
+
+data class RegisterUserRequest(
+    val collegeName: String ?,
+    val department: String ?,
+    val email: String ?,
+    val fullName: String ?,
+    val gender: String ?,
+    val password: String ?,
+    val phone: String ?,
+    val studentId: String?
+)
---
+++ frontend/composeApp/src/commonMain/kotlin/org/rajat/quickpick/domain/modal/auth/RegisterVendorResponse.kt
@@ -0,0 +1,10 @@
+package org.rajat.quickpick.domain.modal.auth
+data class RegisterVendorResponse(
+    val email: String,
+    val message: String,
+    val name: String,
+    val refreshToken: Any,
+    val role: String,
+    val token: Any,
+    val userId: String
+)
